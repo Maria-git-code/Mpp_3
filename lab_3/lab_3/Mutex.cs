3398990f3cd5070eec53ea373f807fff31fd9c47
Maria-git-code <83037470+Maria-git-code@users.noreply.github.com>
2021-12-29 15:45:03 +0300

    Add files via upload

---
+++ lab_3/lab_3/Mutex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace lab_3
+{
+    public class Mutex
+    {
+        private int _lockThreadId ;
+
+        public void Lock()
+        {
+            while(Interlocked.CompareExchange(ref _lockThreadId, Thread.CurrentThread.ManagedThreadId, 0) != 0)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        public void Unlock()
+        {
+            Interlocked.CompareExchange(ref _lockThreadId, 0, Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ lab_3/lab_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace lab_3
+{
+    class Program
+    {
+        private static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+           
+            for(int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(print);
+                thread.Start();
+            }
+            Console.ReadLine();
+        }
+
+        public static void print()
+        {
+            mutex.Lock();
+            Console.WriteLine("Start thread " + Thread.CurrentThread.ManagedThreadId);
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("thread " + Thread.CurrentThread.ManagedThreadId + " step " + i);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Finish thread " + Thread.CurrentThread.ManagedThreadId);
+            mutex.Unlock();
+        }
+    }
+}
